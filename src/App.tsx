51183ac639c2d6edb624ccfbd3776dc04d6e935b
sandeepdreddy <sandeepkduvvur@gmail.com>
2023-02-11 22:56:50 -0800

    base form with react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+import axios from "axios";
+import {transformData} from './util'
+
+const API_URL = 'https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/daily.txt'
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
+    <QueryClientProvider client={queryClient}>
+      <CurrencyForm />
+    </QueryClientProvider>
+  );
+}
+
+function CurrencyForm() {
+
+  const { isLoading, error, data, isFetching } = useQuery("currencyData", () =>
+    axios.get(API_URL)
+    .then((res) => {
+      console.log('Transformed Data:', transformData(res.data))
+      return res.data
+    })
+  )
+
+  if (isLoading) return <p>Loading...</p>
+
+  if (error) return <p>An error has occurred</p>
+  return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{data}</p>
+        <form>
+          <label>
+            Amount to Convert:
+            <input type="number" />
+          </label>
+          <label>
+            Choose the currency to convert to:
+            <select>
+              <option value="USD">US Dollar</option>
+              <option value="INR">Indian Rupees</option>
+              <option value="JPY">Japan Yen</option>
+            </select>
+          </label>
+        </form>
       </header>
     </div>
   );
---
+++ src/util.ts
@@ -0,0 +1,44 @@
+const COLUMN_DELIMITER = '|'
+
+export const HeaderStrings = {
+    COUNTRY: 'Country',
+    CURRENCY: 'Currency',
+    AMOUNT: 'Amount',
+    CODE: 'Code',
+    RATE: 'Rate',
+}
+
+// Fetching indexes in case columns are changed in the
+// repsonse from the API
+const getHeaderIndexes = (headersInfo: string) => {
+    const headers = headersInfo.split(COLUMN_DELIMITER)
+    return {
+        [HeaderStrings.COUNTRY] : headers.indexOf(HeaderStrings.COUNTRY),
+        [HeaderStrings.CURRENCY] : headers.indexOf(HeaderStrings.CURRENCY),
+        [HeaderStrings.AMOUNT] : headers.indexOf(HeaderStrings.AMOUNT),
+        [HeaderStrings.CODE] : headers.indexOf(HeaderStrings.CODE),
+        [HeaderStrings.RATE] : headers.indexOf(HeaderStrings.RATE),
+    }
+}
+
+export const transformData = (currencyData: string) => {
+    const [date, headersInfo, ...rest] = currencyData.split('\n')
+    const headerIndexes = getHeaderIndexes(headersInfo)
+    const conversionData = 
+        rest.filter(conversion => !!conversion)
+            .map((conversion) => {
+                const formattedConversion = conversion.split(COLUMN_DELIMITER)
+                const currencyCode = formattedConversion[headerIndexes[HeaderStrings.CODE]]
+                return {
+                    currencyCode,
+                    country: formattedConversion[headerIndexes[HeaderStrings.COUNTRY]],
+                    currencyString: formattedConversion[headerIndexes[HeaderStrings.CURRENCY]],
+                    amount: formattedConversion[headerIndexes[HeaderStrings.AMOUNT]],
+                    rate: formattedConversion[headerIndexes[HeaderStrings.RATE]],
+                }
+            })
+    return {
+        date,
+        conversionData
+    }
+}
